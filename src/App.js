e08ebd665aefb6a23e2568ae05e64cb7825b5f40
Arturo Castillo <arturguay@gmail.com>
2020-10-25 00:49:47 -0500

    First changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import EventsList from './components/EventList'
 
 function App() {
   return (
     <div className="App">
       <div>Hello</div>
+      <EventsList />
     </div>
   );
 }
---
+++ src/components/EventList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EventList() {
+    return (
+        <div>
+           Hey im events list 
+        </div>
+    )
+}
